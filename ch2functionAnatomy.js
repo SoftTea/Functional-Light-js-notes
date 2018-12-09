55b116c28c4fef2acde61e87e65d5d0709e2fee1
Tim Mickle <TimothyRMickle@gmail.com>
2018-12-09 14:25:56 -0600

    ch2 notes complete

--- ch2functionAnatomy.js
+++ ch2functionAnatomy.js
@@ -42,4 +42,95 @@ foo( [1,2,3] ); // x = 1
                 //  y = 2
                 // args = [3]
 
-// Named Arguments
+// Named Arguments 
+// Destructuring object parameters
+
+
+function foo( {x,y} = {} ) {
+    console.log( x, y );
+}
+
+foo( {y: 3} );   // ==> undefined , 3        
+foo( {y: 3 , x : 1} );   // ==> 1 , 3 
+
+// Explicit Outputs vs Implicit output
+
+// Example of implicit output 
+
+var y;
+
+function f(x) {
+    y = (2 * Math.pow( x, 2 )) + 3;
+}
+
+f( 2 );
+
+y;                      // 11
+
+
+// explicit output 
+
+function f(x) {
+    return (2 * Math.pow( x, 2 )) + 3;
+}
+
+var y = f( 2 );
+
+y;  
+
+// Another example of implicit output 
+//This implicit function output has a special name in the FP world: side effects. 
+// And a function that has no side effects also has a special name: pure function.
+
+function sum(list) {
+    var total = 0;
+    for (let i = 0; i < list.length; i++) {
+        if (!list[i]) list[i] = 0;
+
+        total = total + list[i];
+    }
+
+    return total;
+}
+
+var nums = [ 1, 3, 9, 27, , 84 ];
+
+sum( nums );           // 124
+// Our function sum() implicit output of changing out num variable 
+console.log(nums); // === [ 1, 3, 9, 27, 0, 84];
+
+// Keeping Scope
+
+
+function person(name) {
+    return function identify(){
+        console.log( `I am ${name}` );
+    };
+}
+
+var fred = person( "Fred" );
+var susan = person( "Susan" );
+
+fred();                 // I am Fred
+susan();                // I am Susan
+
+
+function formatter(formatFn) {
+    return function inner(str){
+        return formatFn( str );
+    };
+}
+
+///////////////////////////////////////////////////////////////////////////////
+var lower = formatter( function formatting(v){
+    return v.toLowerCase();
+} );
+
+var upperFirst = formatter( function formatting(v){
+    return v[0].toUpperCase() + v.substr( 1 ).toLowerCase();
+} );
+
+lower( "WOW" );             // wow
+upperFirst( "hello" );      // Hello
+
+
